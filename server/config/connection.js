20dbe40be78fb34155058560902f6ad33f0bda56
Ohlhjames <ohlhjames@gmail.com>
2024-07-22 21:02:05 -0500

    Adjusted file structure to make publishing the web page easier

--- server/config/connection.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-require("dotenv").config(); // Load environment variables
-
-mongoose.connect(process.env.Honkai_DB, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-module.exports = mongoose.connection;
